7dc0b044a9142ab4eb65814dbe7e2c8a37ef4db1
Tarun Saini <sainitarun7888@gmail.com>
2026-06-07 18:09:20 +0530

    Practice Python lists

---
+++ list.py
@@ -0,0 +1,46 @@
+# print("hello python")
+random_list = ["Tarun","Nitish","Manish","Manav","Krish"]
+
+# # Access Elements
+# print(random_list[0])
+
+# # Negatice Indexing (start from last element)
+# print(random_list[-1])
+
+# #update element 
+# random_list[2]="Sahil"
+# print(random_list)
+
+# #add element
+# random_list.append("Manish")
+# print(random_list)
+
+# #clear list
+# random_list.clear()
+# print(random_list)
+
+# #insert element
+# random_list.insert(2,"Aman")
+# print(random_list)
+
+# # remove element
+# random_list.remove("Manish")
+# print(random_list)
+
+# # remove last element
+# random_list.pop()
+# print(random_list)
+
+# # length of list 
+# print(len(random_list))
+
+# # check existence
+# if "Manav" in random_list:
+#     print("Found") 
+
+#----------Slicing----------
+# First 3 elements
+print(random_list[:3])    
+
+# Last 2 elements
+print(random_list[-2:])
