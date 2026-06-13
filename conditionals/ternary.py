ef990565fb9dc643c0c6f873ac61ea4c9ff7d444
Tarun Saini <sainitarun7888@gmail.com>
2026-06-13 23:10:19 +0530

    Ternary operator

---
+++ conditionals/ternary.py
@@ -0,0 +1,10 @@
+#Ternary operator- (value_if_true if condition else value_if_false)
+
+age=18
+
+print("Adul") if age>=18 else print("Minor")
+
+# Checking multiple condition
+age=25
+if 18<= age <=60:
+    print("Working age")
