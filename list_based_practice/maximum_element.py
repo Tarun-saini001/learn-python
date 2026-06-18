1037b2df03fc79777141628f8f52219798c6133e
Tarun Saini <sainitarun7888@gmail.com>
2026-06-18 19:50:25 +0530

    min element in list

--- list_based_practice/maximum_element.py
+++ list_based_practice/maximum_element.py
@@ -8,3 +8,15 @@ def max_value(numbers):
     return largest
 
 print(max_value([20, 40, 30, 100, 100, 2]))
+
+
+def mini_value(numbers):
+    smallest = numbers[0]
+
+    for num in numbers:
+        if num < smallest:
+            smallest = num
+
+    return smallest
+
+print(mini_value([20, 40, 30, 100, 100, 2]))
