39deea634be23c2415add237d3f3f3cd2128ad77
Tarun Saini <sainitarun7888@gmail.com>
2026-06-18 20:00:20 +0530

    count even num in list

---
+++ list_based_practice/count_even.py
@@ -0,0 +1,14 @@
+def count_even(numbers):
+    count=0
+    for num in numbers:
+        if(num%2==0):
+            count+=1
+    return count
+
+print(count_even([1,2,3,4,5,6,7]))
+
+
+# def count_even(numbers):
+#     return sum(1 for num in numbers if num % 2 == 0) #"For every number in numbers, if it is even, produce a 1."
+
+# print(count_even([1, 2, 3, 4, 5, 6, 7]))
