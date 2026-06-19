5649d209f533c368bb1f82f11952685d411b05fd
Tarun Saini <sainitarun7888@gmail.com>
2026-06-19 17:36:29 +0530

    count unique characters

--- strings/practice/count_char.py
+++ strings/practice/count_char.py
@@ -7,3 +7,15 @@ def count_char(string):
     return count
 
 print(count_char("Hello   "))    
+
+#Count unique
+
+def count_unique_chars(string):
+    unique_chars=[]
+    for char in string:
+        if char != " " and char not in unique_chars:
+            unique_chars.append(char)
+            
+    return len(unique_chars)  
+
+print(count_unique_chars("aaaaabc"))     
