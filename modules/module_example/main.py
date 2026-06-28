2e5ef7ef8a790b0b5c5d4f6d47e4b5af20a38917
Tarun Saini <sainitarun7888@gmail.com>
2026-06-28 13:21:47 +0530

    modules

---
+++ modules/module_example/main.py
@@ -0,0 +1,7 @@
+import math_utils
+from math_utils import add  #import specific function
+
+ 
+print(math_utils.multiply(5,5))
+
+print(add(4,4))
---
+++ modules/module_intro.py
@@ -0,0 +1,17 @@
+# A module is simply a Python file (.py).
+
+# Example:
+
+# math_utils.p
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b        #This file is a module.
+
+
+# Importing a Module
+# main.py
+
+# import math_utils
+# print(math_utils.add(10, 20))
